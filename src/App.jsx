e1f9b835ab89b15b15288650750517956a4539f4
Jacob Aguilar <aguilar.campos.jacob@gmail.com>
2024-06-09 00:38:10 -0400

    Add base navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
-import './App.css'
+import './App.css';
+import logo from './assets/sonkei-logo.webp';
 
-function App() {
+const App = () => {
   return (
     <>
-     
+      <nav className="no-underline flex flex-row justify-between items-center text-wrap w-full sticky text-white font-bold p-2 bg-black z-10">
+        <a href="/"><img src={logo} alt="sonkei fc logo" className='size-14 cursor-pointer'/></a>
+
+        <ul className="flex flex-row">
+          <li className="hover:text-black px-4 py-2 rounded-lg transition scale-100 hover:scale-110 hover:bg-yellow">
+            <a href="#inicio">Inicio</a>
+          </li>
+          <li className="hover:text-black px-4 py-2 rounded-lg transition scale-100 hover:scale-110 hover:bg-yellow">
+            <a href="#equipo">Equipo</a>
+          </li>
+          <li className="hover:text-black px-4 py-2 rounded-lg transition scale-100 hover:scale-110 hover:bg-yellow">
+            <a href="#historia" >Nuestra Historia</a>
+          </li>
+          <li className="hover:text-black px-4 py-2 rounded-lg transition scale-100 hover:scale-110 hover:bg-yellow">
+            <a href="#entrenamiento">Entrenamiento</a>
+          </li>
+          <li className="hover:text-black px-4 py-2 rounded-lg transition scale-100 hover:scale-110 hover:bg-yellow">
+            <a href="#testimonios">Testimonios</a>
+          </li>
+          <li className="hover:text-black px-4 py-2 rounded-lg transition scale-100 hover:scale-110 hover:bg-yellow">
+            <a href="#contacto">Contacto</a>
+          </li>
+        </ul>
+      </nav>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
