cc2b8520b8196ebb87be9f414b1c881e52700fd1
Jacob Aguilar <aguilar.campos.jacob@gmail.com>
2024-06-10 17:34:42 -0400

    Add duration to hover for each nav bar option

--- src/App.jsx
+++ src/App.jsx
@@ -9,22 +9,22 @@ const App = () => {
         <a href="/"><img src={logo} alt="sonkei fc logo" className='size-14 cursor-pointer'/></a>
 
         <ul className="flex flex-row">
-          <li className="hover:text-black px-4 py-2 rounded-lg transition scale-100 hover:scale-110 hover:bg-yellow">
+          <li className="hover:text-black px-4 py-2 rounded-lg transition duration-300 scale-100 hover:duration-500 hover:scale-110 hover:bg-yellow">
             <a href="#inicio">Inicio</a>
           </li>
-          <li className="hover:text-black px-4 py-2 rounded-lg transition scale-100 hover:scale-110 hover:bg-yellow">
+          <li className="hover:text-black px-4 py-2 rounded-lg transition duration-300 scale-100 hover:duration-500 hover:scale-110 hover:bg-yellow">
             <a href="#equipo">Equipo</a>
           </li>
-          <li className="hover:text-black px-4 py-2 rounded-lg transition scale-100 hover:scale-110 hover:bg-yellow">
+          <li className="hover:text-black px-4 py-2 rounded-lg transition duration-300 scale-100 hover:duration-500 hover:scale-110 hover:bg-yellow">
             <a href="#historia" >Nuestra Historia</a>
           </li>
-          <li className="hover:text-black px-4 py-2 rounded-lg transition scale-100 hover:scale-110 hover:bg-yellow">
+          <li className="hover:text-black px-4 py-2 rounded-lg transition duration-300 scale-100 hover:duration-500 hover:scale-110 hover:bg-yellow">
             <a href="#entrenamiento">Entrenamiento</a>
           </li>
-          <li className="hover:text-black px-4 py-2 rounded-lg transition scale-100 hover:scale-110 hover:bg-yellow">
+          <li className="hover:text-black px-4 py-2 rounded-lg transition duration-300 scale-100 hover:duration-500 hover:scale-110 hover:bg-yellow">
             <a href="#testimonios">Testimonios</a>
           </li>
-          <li className="hover:text-black px-4 py-2 rounded-lg transition scale-100 hover:scale-110 hover:bg-yellow">
+          <li className="hover:text-black px-4 py-2 rounded-lg transition duration-300 scale-100 hover:duration-500 hover:scale-110 hover:bg-yellow">
             <a href="#contacto">Contacto</a>
           </li>
         </ul>
